c2bf7dffdca32fd53598c4eb9dd43ada0afbbc1c
HAJIME Fukuna <f97one@gmail.com>
2014-03-14 23:05:13 +0900

    アプリのキモになるBroadcastReceiverとServiceを継承したクラスを追加。

---
+++ android/src/main/java/net/formula97/android/screenkeeper/SensorManagerService.java
@@ -0,0 +1,35 @@
+package net.formula97.android.screenkeeper;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by HAJIME on 14/03/14.
+ */
+public class SensorManagerService extends Service {
+    /**
+     * Return the communication channel to the service.  May return null if
+     * clients can not bind to the service.  The returned
+     * {@link android.os.IBinder} is usually for a complex interface
+     * that has been <a href="{@docRoot}guide/components/aidl.html">described using
+     * aidl</a>.
+     * <p/>
+     * <p><em>Note that unlike other application components, calls on to the
+     * IBinder interface returned here may not happen on the main thread
+     * of the process</em>.  More information about the main thread can be found in
+     * <a href="{@docRoot}guide/topics/fundamentals/processes-and-threads.html">Processes and
+     * Threads</a>.</p>
+     *
+     * @param intent The Intent that was used to bind to this service,
+     *               as given to {@link android.content.Context#bindService
+     *               Context.bindService}.  Note that any extras that were included with
+     *               the Intent at that point will <em>not</em> be seen here.
+     * @return Return an IBinder through which clients can call on to the
+     * service.
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
