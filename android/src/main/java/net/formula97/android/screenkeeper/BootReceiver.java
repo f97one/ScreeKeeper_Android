dc674651876f586234ae8e74e83eedf91f6e0722
HAJIME Fukuna <f97one@gmail.com>
2014-03-14 23:28:41 +0900

    ブロードキャストを受信したときの処理を追加。

--- android/src/main/java/net/formula97/android/screenkeeper/BootReceiver.java
+++ android/src/main/java/net/formula97/android/screenkeeper/BootReceiver.java
@@ -3,48 +3,31 @@ package net.formula97.android.screenkeeper;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
 /**
- * Created by HAJIME on 14/03/14.
+ * スタートアップ起動を行うBroadcastReceiver。<br />
+ * Created by HAJIME Fukuna on 14/03/14.
  */
 public class BootReceiver extends BroadcastReceiver {
     /**
-     * This method is called when the BroadcastReceiver is receiving an Intent
-     * broadcast.  During this time you can use the other methods on
-     * BroadcastReceiver to view/modify the current result values.  This method
-     * is always called within the main thread of its process, unless you
-     * explicitly asked for it to be scheduled on a different thread using
-     * {@link android.content.Context#registerReceiver(android.content.BroadcastReceiver,
-     * IntentFilter, String, android.os.Handler)}. When it runs on the main
-     * thread you should
-     * never perform long-running operations in it (there is a timeout of
-     * 10 seconds that the system allows before considering the receiver to
-     * be blocked and a candidate to be killed). You cannot launch a popup dialog
-     * in your implementation of onReceive().
-     * <p/>
-     * <p><b>If this BroadcastReceiver was launched through a &lt;receiver&gt; tag,
-     * then the object is no longer alive after returning from this
-     * function.</b>  This means you should not perform any operations that
-     * return a result to you asynchronously -- in particular, for interacting
-     * with services, you should use
-     * {@link android.content.Context#startService(android.content.Intent)} instead of
-     * {@link android.content.Context#bindService(android.content.Intent, ServiceConnection, int)}.  If you wish
-     * to interact with a service that is already running, you can use
-     * {@link #peekService}.
-     * <p/>
-     * <p>The Intent filters used in {@link android.content.Context#registerReceiver}
-     * and in application manifests are <em>not</em> guaranteed to be exclusive. They
-     * are hints to the operating system about how to find suitable recipients. It is
-     * possible for senders to force delivery to specific recipients, bypassing filter
-     * resolution.  For this reason, {@link #onReceive(android.content.Context, android.content.Intent) onReceive()}
-     * implementations should respond only to known actions, ignoring any unexpected
-     * Intents that they may receive.
+     * ACTION_BOOT_COMPLETEDブロードキャストをトリガーに、センサー検知サービスを起動する。<br />
      *
      * @param context The Context in which the receiver is running.
      * @param intent  The Intent being received.
+     * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
      */
     @Override
     public void onReceive(Context context, Intent intent) {
+        // Preferenceの値を参照する
+        SharedPreferences preference = context.getSharedPreferences("ScreenKeeperPreference", Context.MODE_PRIVATE);
 
+        // スタートアップ起動を、StartAfterBootがtrueの場合だけに限定する
+        if (preference.getBoolean("StartAfterBoot", false)) {
+            String action = intent.getAction();
+            if (action.equals(Intent.ACTION_BOOT_COMPLETED)) {
+                context.startService(new Intent(context, SensorManagerService.class));
+            }
+        }
     }
 }
