09c456e61190658bd95b92a563e5fe986aa779d1
HAJIME Fukuna <f97one@gmail.com>
2014-03-17 23:04:19 +0900

    傾きセンサー使用のためのインターフェイスを実装。

--- android/src/main/java/net/formula97/android/screenkeeper/SensorManagerService.java
+++ android/src/main/java/net/formula97/android/screenkeeper/SensorManagerService.java
@@ -6,13 +6,16 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.os.Handler;
 import android.os.IBinder;
 
 /**
  * Created by HAJIME on 14/03/14.
  */
-public class SensorManagerService extends Service {
+public class SensorManagerService extends Service implements SensorEventListener {
 
     Handler mHandler;
 
@@ -104,5 +107,37 @@ public class SensorManagerService extends Service {
         } else {
             builder.setContentText(getString(R.string.not_activated));
         }
+    }
+
+    /**
+     * Called when sensor values have changed.
+     * <p>See {@link android.hardware.SensorManager SensorManager}
+     * for details on possible sensor types.
+     * <p>See also {@link android.hardware.SensorEvent SensorEvent}.
+     * <p/>
+     * <p><b>NOTE:</b> The application doesn't own the
+     * {@link android.hardware.SensorEvent event}
+     * object passed as a parameter and therefore cannot hold on to it.
+     * The object may be part of an internal pool and may be reused by
+     * the framework.
+     *
+     * @param event the {@link android.hardware.SensorEvent SensorEvent}.
+     */
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        
+    }
+
+    /**
+     * Called when the accuracy of a sensor has changed.
+     * <p>See {@link android.hardware.SensorManager SensorManager}
+     * for details.
+     *
+     * @param sensor
+     * @param accuracy The new accuracy of this sensor
+     */
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
