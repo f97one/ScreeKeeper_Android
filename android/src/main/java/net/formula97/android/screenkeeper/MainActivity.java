9682bd0c7e27f9bbe90067d0a5e6ce7914f2e63f
HAJIME Fukuna <f97one@gmail.com>
2014-03-23 19:08:22 +0900

    サービスの死活判断をする処理を、別クラスに移動。

--- android/src/main/java/net/formula97/android/screenkeeper/MainActivity.java
+++ android/src/main/java/net/formula97/android/screenkeeper/MainActivity.java
@@ -1,16 +1,12 @@
 package net.formula97.android.screenkeeper;
 
 import android.app.Activity;
-import android.app.ActivityManager;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-
-import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -34,8 +30,11 @@ public class MainActivity extends Activity {
         SharedPreferences pref = getPref();
         cb_startUp.setChecked(pref.getBoolean("StartAfterBoot", false));
 
+        final SvcUtil util = new SvcUtil(getApplicationContext());
+        final String keeper = SensorManagerService.class.getCanonicalName();
+
         // サービスの実行状況に応じて、ボタンのキャプションを変更する
-        if (isKeeperRunning()) {
+        if (util.isKeeperRunning(keeper)) {
             btn_startStopManually.setText(R.string.start_manually);
         } else {
             btn_startStopManually.setText(R.string.stop_manually);
@@ -47,7 +46,7 @@ public class MainActivity extends Activity {
             Intent intent = new Intent(getApplicationContext(), SensorManagerService.class);
             @Override
             public void onClick(View v) {
-                if (isKeeperRunning()) {
+                if (util.isKeeperRunning(keeper)) {
                     startService(intent);
                     btn_startStopManually.setText(R.string.stop_manually);
                 } else {
@@ -76,24 +75,4 @@ public class MainActivity extends Activity {
         return getSharedPreferences("ScreenKeeper_pref", MODE_PRIVATE);
     }
 
-    /**
-     * SensorManagerServiceが実行中かどうかを判断する。
-     * @return boolean型、実行中ならtrue、停止中ならfalseを返す。
-     */
-    private boolean isKeeperRunning() {
-        boolean ret = false;
-
-        String svcname = SensorManagerService.class.getCanonicalName();
-        ActivityManager manager = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.RunningServiceInfo> services = manager.getRunningServices(Integer.MAX_VALUE);
-
-        for (ActivityManager.RunningServiceInfo info : services) {
-            if (svcname.equals(info.service.getClassName())) {
-                ret = true;
-                break;
-            }
-        }
-
-        return ret;
-    }
 }
--- android/src/main/java/net/formula97/android/screenkeeper/SvcUtil.java
+++ android/src/main/java/net/formula97/android/screenkeeper/SvcUtil.java
@@ -1,7 +1,47 @@
 package net.formula97.android.screenkeeper;
 
+import android.app.ActivityManager;
+import android.content.Context;
+
+import java.util.List;
+
 /**
  * Created by f97one on 14/03/23.
  */
 public class SvcUtil {
+
+    Context mContext;
+
+    private Context getContext() {
+        return mContext;
+    }
+
+    private void setContext(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public SvcUtil(Context context) {
+        setContext(context);
+    }
+
+    /**
+     * SensorManagerServiceが実行中かどうかを判断する。
+     * @return boolean型、実行中ならtrue、停止中ならfalseを返す。
+     */
+    public boolean isKeeperRunning(String serviceCanonicalName) {
+        boolean ret = false;
+
+        ActivityManager manager = (ActivityManager)mContext.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> services = manager.getRunningServices(Integer.MAX_VALUE);
+
+        for (ActivityManager.RunningServiceInfo info : services) {
+            if (serviceCanonicalName.equals(info.service.getClassName())) {
+                ret = true;
+                break;
+            }
+        }
+
+        return ret;
+    }
+
 }
