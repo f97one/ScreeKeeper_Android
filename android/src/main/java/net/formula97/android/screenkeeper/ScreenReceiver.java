73c8ebe5c729d63c39e1568431c7659f1cb69add
HAJIME Fukuna <f97one@gmail.com>
2014-03-23 18:46:30 +0900

    スクリーンの点灯／消灯を検知する目的のレシーバーを追加。

---
+++ android/src/main/java/net/formula97/android/screenkeeper/ScreenReceiver.java
@@ -0,0 +1,17 @@
+package net.formula97.android.screenkeeper;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ScreenReceiver extends BroadcastReceiver {
+    public ScreenReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
