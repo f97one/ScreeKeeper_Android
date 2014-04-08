1431e03491e390b3eee2a72c69d217f777d765aa
HAJIME Fukuna <f97one@gmail.com>
2014-04-08 23:31:03 +0900

    タイムアウトまわりの設定値を追加。

--- android/src/main/java/net/formula97/android/screenkeeper/Consts.java
+++ android/src/main/java/net/formula97/android/screenkeeper/Consts.java
@@ -9,9 +9,11 @@ class Consts {
 		static final String START_AFTER_BOOT = "StartAfterBoot";
 		static final String MINIMUM_PITCH = "MinimumPitch";
 		static final String MAXIMUM_PITCH = "MaximumPitch";
+		static final String ACQUIRE_TIMEOUT = "AcquireTimeout";
 		static final int MAX_PITCH_OFFSET = 45;
 		static final int DEFAULT_MAX_PITCH = 35;
 		static final int DEFAULT_MIN_PITCH = 5;
+		static final int DEFAULT_ACQUIRE_TIMEOUT = 180;
 	}
 
 	static final String WAKE_LOCK_TAG = "net.formula97.android.screenkeeper.ACTION_SCREEN_KEEP";
